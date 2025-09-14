b5bb881d270014e6843b7d1b7e0fcf4ef3dbc8f9
Donizete J. R. Vida <41035417+DonizeteVida@users.noreply.github.com>
2025-09-14 23:03:36 +0200

    Update setup.sh

--- amd64_debian/setup.sh
+++ amd64_debian/setup.sh
@@ -4,7 +4,7 @@ mkdir -p ~/sysroots/amd64
 
 sudo debootstrap --arch amd64 stable ~/sysroots/amd64 https://deb.debian.org/debian
 
-cp /usr/bin/qemu-x86_64-static ~/sysroots/amd64/usr/bin/
+sudo cp /usr/bin/qemu-x86_64-static ~/sysroots/amd64/usr/bin/
 
 sudo chroot ~/sysroots/amd64 /debootstrap/debootstrap --second-stage
 
