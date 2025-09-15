510f8cf2abfca846ecddde403ca2b49a02bf17f4
Donizete J. R. Vida <41035417+DonizeteVida@users.noreply.github.com>
2025-09-15 21:38:55 +0200

    Update setup.sh

--- amd64_debian/setup.sh
+++ amd64_debian/setup.sh
@@ -1,8 +1,8 @@
-sudo apt install debootstrap qemu-user-static -y
+sudo apt install debootstrap qemu-user-static binfmt-support -y
 
 mkdir -p ~/sysroots/amd64
 
-sudo debootstrap --arch amd64 stable ~/sysroots/amd64 https://deb.debian.org/debian
+sudo debootstrap --arch amd64 --foreign stable ~/sysroots/amd64 https://deb.debian.org/debian
 
 sudo cp /usr/bin/qemu-x86_64-static ~/sysroots/amd64/usr/bin/
 
